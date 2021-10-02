2564c0ab4ba882c729fbb70d348952f8d121d63a
Pascal Schröder <pascal.schroeder@de.ibm.com>
2021-10-02 20:13:10 +0200

    Add canonical link

--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -23,10 +23,6 @@ export class StartComponent implements OnInit {
   constructor(private router: Router, @Inject(DOCUMENT) private doc) { }
 
   ngOnInit(): void {
-    let link: HTMLLinkElement = this.doc.createElement('link');
-    link.setAttribute('rel', 'canonical');
-    this.doc.head.appendChild(link);
-    link.setAttribute('href', this.doc.URL);
     this.startSliderSubscriber();
     this.text=start;
   }
