ac11a6c2227d0b805a37b06b46bd18d50cf1c66b
Pascal Schröder <pascal.schroeder@de.ibm.com>
2022-01-17 22:29:19 +0100

    Fix netlify forms

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -7,10 +7,12 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatButtonModule} from '@angular/material/button';
+import { NetlifyFormsService } from '../netlify/netlify-forms.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs/internal/Observable';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
+import { ContactMsg } from './contactForm';
 
 @Component({
   selector: 'app-contact',
@@ -24,7 +26,7 @@ export class ContactComponent implements OnInit {
   package;
   concern;
 
-  constructor(private route: ActivatedRoute, private router: Router, private formBuilder: FormBuilder, private http: HttpClient) {
+  constructor(private route: ActivatedRoute, private router: Router, private formBuilder: FormBuilder, private http: HttpClient, private netlifyForms: NetlifyFormsService) {
     this.text=contact;
       this.formGroup = this.formBuilder.group({
         name: ['', Validators.required], 
@@ -54,42 +56,32 @@ export class ContactComponent implements OnInit {
       this.router.navigate(["thanks"])
     }*/
     console.log("submit form")
-    this.submitFeedback(value)
-    this.router.navigate(["thanks"])
-  }
 
-  submitFeedback(value): Observable<any> {
-    const entry = new HttpParams({ fromObject: {
-      'form-name': 'contactFormNetlify',
-      ...value,
-    }});
+    const data = {
+      name: value["name"],
+      email: value["email"],
+      package: value["package"],
+      concern: value["concern"],
+      message: value["message"],
+      terms: value["terms"],
+    };
 
-    return this.submitEntry(entry);
-  }
+    const entry = {
+      ...data,
+    } as ContactMsg;
 
-  private submitEntry(entry: HttpParams): Observable<any> {
-    console.log(entry)
-    return this.http.post(
-      '/',
-      entry.toString(),
-      {
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        responseType: 'text'
+    this.netlifyForms.submitFeedback(entry).subscribe(
+      (res) => {
+        console.log(res)
+        this.router.navigate(["thanks"])
+
+      },
+      (err) => {
+        console.log(err)
+        this.router.navigate(["thanks"])
       }
-    ).pipe(catchError(this.handleError));
+    );
   }
 
-  private handleError(err: HttpErrorResponse) {
-    let errMsg = '';
-
-    if (err.error instanceof ErrorEvent) {
-      errMsg = `A client-side error occurred: ${err.error.message}`;
-    } else {
-      errMsg = `A server-side error occurred. Code: ${err.status}. Message: ${err.message}`;
-    }
-
-    console.log(errMsg)
-    return throwError(errMsg);
-  }
-
+  
 }
---
+++ src/app/contact/contactForm.ts
@@ -0,0 +1,8 @@
+export interface ContactMsg {
+    name: string;
+    email: string;
+    package: string;
+    concern: string;
+    message: string;
+    terms: string;
+  }
