ebda0000080b4065a6a83ec8e75ff344ba80848a
Pascal Schröder <pascal.schroeder@de.ibm.com>
2022-01-17 20:14:41 +0100

    Add netlify form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import contact from '../../assets/text/contact.json'
@@ -8,6 +8,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatButtonModule} from '@angular/material/button';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs/internal/Observable';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Component({
   selector: 'app-contact',
@@ -44,13 +47,45 @@ export class ContactComponent implements OnInit {
   }
 
   onSubmit(value) {
-    if(value.terms==true && this.formGroup.valid) {
+    /*if(value.terms==true && this.formGroup.valid) {
       const headers = new HttpHeaders().set('Content-Type', 'application/json; charset=utf-8');
       this.http.post<any>('https://submit-form.com/JR2hmhGc', JSON.stringify(value), {headers: headers}).subscribe(data => {
       });
       this.router.navigate(["thanks"])
+    }*/
+    this.submitFeedback(value)
+  }
+
+  submitFeedback(value): Observable<any> {
+    const entry = new HttpParams({ fromObject: {
+      'form-name': 'contactFormNetlify',
+      ...value,
+    }});
+
+    return this.submitEntry(entry);
+  }
+
+  private submitEntry(entry: HttpParams): Observable<any> {
+    return this.http.post(
+      '/',
+      entry.toString(),
+      {
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        responseType: 'text'
+      }
+    ).pipe(catchError(this.handleError));
+  }
+
+  private handleError(err: HttpErrorResponse) {
+    let errMsg = '';
+
+    if (err.error instanceof ErrorEvent) {
+      errMsg = `A client-side error occurred: ${err.error.message}`;
+    } else {
+      errMsg = `A server-side error occurred. Code: ${err.status}. Message: ${err.message}`;
     }
-    
+
+    return throwError(errMsg);
   }
 
 }
